0093d7233ea4bc21d20399cf2b32db2fcd18e48f
shuiruohanyu <shuiruohanyu@foxmail.com>
2020-03-15 15:17:01 +0800

    完成Layout组件

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import Vant from 'vant' // 引入vant组件库
 import 'vant/lib/index.css' // 引入vant组件的样式
+import '@/styles/index.less' // 引入自定义的全局样式  覆盖vant的样式
 import 'amfe-flexible'
 Vue.use(Vant) // 注册Vant的所有组件 一旦注册任意位置都可以使用vant的组件 相当于调用了vant的install方法吧
 // 以上做法是完整导入的模式
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>公共布局页面</div>
+  <div class='container'>
+    <!-- 导航组件 采用vant组件的导航  fixed 表示固定在顶部-->
+    <!-- 当页面地址是 /user的时候 应该隐藏掉 导航行 否则不隐藏 -->
+    <!-- @事件名="逻辑" -->
+    <van-nav-bar @click-right="$router.push('/search')" fixed title="黑马头条" right-text="搜索" v-if="$route.path!== '/user'"></van-nav-bar>
+    <!-- 二级路由容器 -->
+    <!-- 判断当前的地址 如果是 /user 就给一个 noTop Class -->
+    <div class="my-wrapper" :class="{noTop: $route.path === '/user'}">
+      <router-view></router-view>
+    </div>
+    <!-- 标签栏组件 -->
+    <!-- 表示开启路由模式 -->
+    <van-tabbar route>
+      <!-- 应该点击导航的标签跳转路由 -->
+      <!-- 一旦启用路由模式 van-tabbar-item 就可以当做router-link使用 -->
+        <van-tabbar-item icon="wap-home-o" to="/">首页</van-tabbar-item>
+        <van-tabbar-item icon="chat-o" to='/question'>问答</van-tabbar-item>
+        <van-tabbar-item icon="tv-o" to="/video">视频</van-tabbar-item>
+        <van-tabbar-item icon="user-circle-o" to="/user">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
 </template>
 
 <script>
@@ -8,6 +28,22 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.container{
+  width: 100%;
+  height: 100%;
+  position: relative;
+  // 控制二级路由容器的高宽
+  .my-wrapper{
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    padding-top: 46px;
+    padding-bottom: 50px;
+    box-sizing: border-box;
+    &.noTop{
+      padding-top: 0;
+    }
+  }
+}
 </style>
