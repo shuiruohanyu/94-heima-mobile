189c1c93ed71ede200d94168357533b715dcbc6e
shuiruohanyu <shuiruohanyu@foxmail.com>
2020-03-16 15:59:20 +0800

    登录-基础布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,6 +4,21 @@
     <!-- click-left点击左侧事件 -->
     <!-- $router.go(-1) $router.back() -->
     <van-nav-bar title='登录'  left-arrow @click-left="$router.back()"></van-nav-bar>
+    <!-- 登录布局 -->
+    <!-- 外层需要用cell-group组件来包裹提供 边框 -->
+    <van-cell-group>
+        <!-- 登录手机号 -->
+          <van-field label="手机号"  placeholder="请输入手机号"></van-field>
+        <!-- 验证码 -->
+          <van-field label="验证码"  placeholder="请输入验证码">
+            <!-- 插槽内容 -->
+              <van-button slot="button" size="small" type="primary">发送验证码</van-button>
+          </van-field>
+    </van-cell-group>
+    <!-- 登录按钮 -->
+    <div class='login-box'>
+      <van-button type="info" round size="small" block>登录</van-button>
+    </div>
   </div>
 </template>
 
@@ -14,5 +29,7 @@ export default {
 </script>
 
 <style>
-
+.login-box {
+  padding: 20px
+}
 </style>
