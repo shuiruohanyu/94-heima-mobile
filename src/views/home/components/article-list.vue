34a68c54d18665c9098c72de90896f1460c91dfd
shuiruohanyu <shuiruohanyu@foxmail.com>
2020-03-18 16:12:56 +0800

    封装article-list组件

---
+++ src/views/home/components/article-list.vue
@@ -0,0 +1,40 @@
+<template>
+  <!-- 文章列表组件  放置列表 -->
+  <!-- van-list 可以帮助我们实现上拉加载  需要一些变量 -->
+  <!-- 这里放置这个div的目的是 形成滚动条, 因为我们后期要做 阅读记忆 -->
+  <!-- 阅读记忆  上次你阅读到哪  回来之后还是哪-->
+  <div class="scroll-wrapper">
+      <!-- 文章列表 -->
+      <!-- van-list组件 如果不加干涉, 初始化完毕 就会检测 自己距离底部的长度,如果超过了限定 ,就会执行 load事件  自动把
+       绑定的 loading 变成true
+      -->
+    <van-list v-model="upLoading" :finished="finished" @load="onLoad"></van-list>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      upLoading: false, // 表示是否开启了上拉加载 默认值false
+      finished: false // 表示 是否已经完成所有数据的加载
+    }
+  },
+  methods: {
+    onLoad () {
+      console.log('开始加载数据')
+      // 下面这么写 依然不能关掉加载状态 为什么 ? 因为关掉之后  检测机制  高度还是不够 还是会开启
+      // 如果你有数据 你应该 把数据到加到list中
+      // 如果想关掉
+      setTimeout(() => {
+        this.finished = true // 表示 数据已经全部加载完毕 没有数据了
+      }, 1000) // 等待一秒 然后关闭加载状态
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -7,12 +7,13 @@
          <van-tab :title="`标签${item}`" v-for="item in 10" :key="item">
             <!-- 生成若干个单元格 -->
             <!-- 这里为什么叫scroll-wrapper  因为样式中对它进行了设置 -->
-            <div class='scroll-wrapper'>
+            <!-- <div class='scroll-wrapper'>
               <van-cell-group>
                 <van-cell title="标题" value="内容" :key="item" v-for="item in 20"></van-cell>
               </van-cell-group>
-            </div>
-
+            </div> -->
+            <!-- 有多少个tab 就有多少个 article-list  相当于多个article-list实例-->
+             <ArticleList></ArticleList>
          </van-tab>
       </van-tabs>
       <!-- 在tabs下放置图标  编辑频道的图标 -->
@@ -25,10 +26,12 @@
 
 <script>
 // @ is an alias to /src
-
+import ArticleList from './components/article-list'
 export default {
   name: 'Home',
-  components: {}
+  components: {
+    ArticleList
+  }
 }
 </script>
 <style lang='less' scoped>
