72655080f4764585746883b97180c68e8eb3de6d
shuiruohanyu <shuiruohanyu@foxmail.com>
2020-03-19 10:01:32 +0800

    article-list实现下拉刷新

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -4,16 +4,19 @@
   <!-- 这里放置这个div的目的是 形成滚动条, 因为我们后期要做 阅读记忆 -->
   <!-- 阅读记忆  上次你阅读到哪  回来之后还是哪-->
   <div class="scroll-wrapper">
-      <!-- 文章列表 -->
-      <!-- van-list组件 如果不加干涉, 初始化完毕 就会检测 自己距离底部的长度,如果超过了限定 ,就会执行 load事件  自动把
+    <!-- 文章列表 -->
+    <!-- van-list组件 如果不加干涉, 初始化完毕 就会检测 自己距离底部的长度,如果超过了限定 ,就会执行 load事件  自动把
        绑定的 loading 变成true
-      -->
-    <van-list  finished-text='没有了' v-model="upLoading" :finished="finished" @load="onLoad">
-      <!-- 循环内容 -->
-      <van-cell-group>
-        <van-cell v-for="item in articles" :key="item" title="美股又熔断了" :value="'天台排队'+item"></van-cell>
-      </van-cell-group>
-    </van-list>
+    -->
+    <!-- 下拉刷新组件包裹 列表组件 -->
+    <van-pull-refresh v-model="downLoading" @refresh="onRefresh" :success-text="successText">
+      <van-list finished-text="没有了" v-model="upLoading" :finished="finished" @load="onLoad">
+        <!-- 循环内容 -->
+        <van-cell-group>
+          <van-cell v-for="item in articles" :key="item" title="美股又熔断了" :value="'天台排队'+item"></van-cell>
+        </van-cell-group>
+      </van-list>
+    </van-pull-refresh>
   </div>
 </template>
 
@@ -21,6 +24,8 @@
 export default {
   data () {
     return {
+      successText: '', // 刷新成功的文本
+      downLoading: false, // 下载刷新状态 表示是否正在下拉刷新
       upLoading: false, // 表示是否开启了上拉加载 默认值false
       finished: false, // 表示 是否已经完成所有数据的加载
       articles: [] // 文章列表
@@ -28,6 +33,7 @@ export default {
   },
   methods: {
     // onLoad 是会自动执行
+    // 上拉加载
     onLoad () {
       console.log('开始加载数据')
       // 如果你的数据已经加载完毕 你应该把finished 设置为true 表示一切结束了 不再请求
@@ -38,7 +44,10 @@ export default {
         this.finished = true // 关闭加载
       } else {
         // 1-60
-        const arr = Array.from(Array(15), (value, index) => this.articles.length + index + 1)
+        const arr = Array.from(
+          Array(15),
+          (value, index) => this.articles.length + index + 1
+        )
         // 上拉加载 不是覆盖之前的数据  应该把数据追加到数组的队尾
         this.articles.push(...arr)
         // 添加完数据 需要手动的关掉 loading
@@ -51,12 +60,22 @@ export default {
       // setTimeout(() => {
       //   this.finished = true // 表示 数据已经全部加载完毕 没有数据了
       // }, 1000) // 等待一秒 然后关闭加载状态
+    },
+    // 下拉刷新
+    onRefresh () {
+      setTimeout(() => {
+      // 下拉刷新 表示要读取最新的数据 而且最新的数据要添加到数据头部
+        const arr = Array.from(Array(2), (value, index) => '追加' + (index + 1))
+        // 数组添加到头部
+        this.articles.unshift(...arr)
+        // 手动关闭正在加载的状态
+        this.downLoading = false
+        this.successText = `更新了${arr.length}条数据`
+      }, 1000)
     }
   }
-
 }
 </script>
 
 <style>
-
 </style>
