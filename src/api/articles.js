75f07ef3dd981510cd65f64ad2afa61f1d7eb799
shuiruohanyu <shuiruohanyu@foxmail.com>
2020-03-19 11:40:42 +0800

    封装获取文章列表api

---
+++ src/api/articles.js
@@ -0,0 +1,17 @@
+/****
+ * 处理文章模块的请求数据
+ *
+ * ***/
+import request from '@/utils/request'
+
+/****
+ * 获取文章推荐数据
+ *  需要传入 timestamp 时间戳  channel_id (频道id)
+ * ****/
+export function getArticles (params) {
+  return request({
+    // 这里必须用完整地址 因为 之前设置的baseURL是 v1_0
+    url: 'http://ttapi.research.itcast.cn/app/v1_1/articles',
+    params: { with_top: 1, ...params } // 合并 数据
+  })
+}
