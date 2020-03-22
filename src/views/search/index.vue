b441667b9b6955e7c7f88b64d4beabf216c22727
shuiruohanyu <shuiruohanyu@foxmail.com>
2020-03-22 15:48:15 +0800

    搜索中心-基础布局

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,18 +1,70 @@
 <template>
-  <div class='container'>
-    <!-- 导航 显示返回箭头-->
-    <!-- click-left点击左侧事件 -->
-    <!-- $router.go(-1) $router.back() -->
-    <van-nav-bar title='搜索中心' left-arrow @click-left="$router.back()"></van-nav-bar>
+  <div class="container">
+    <!-- 搜索组件一级路由   返回上一个页面-->
+    <van-nav-bar left-arrow title="搜索中心" @click-left="$router.back()"></van-nav-bar>
+    <!-- 导航 -->
+    <!-- 搜索组件 -->
+    <van-search  placeholder="请输入搜索关键词" shape="round" />
+    <!-- 联想内容  -->
+    <van-cell-group class="suggest-box" >
+      <van-cell icon="search">
+        <span>j</span>ava
+      </van-cell>
+    </van-cell-group>
+    <!-- 历史记录部分  你搜索的内容 会在这里记录 -->
+    <div class="history-box">
+      <div class="head">
+        <span>历史记录</span>
+        <van-icon name="delete"></van-icon>
+      </div>
+      <van-cell-group>
+        <van-cell>
+          <a class="word_btn">电脑</a>
+          <van-icon class="close_btn" slot="right-icon" name="cross" />
+        </van-cell>
+      </van-cell-group>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: 'search'
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.history-box {
+  padding: 0 20px;
+  .head{
+    line-height: 36px;
+    color: #999;
+    .van-icon{
+      font-size: 16px;
+      float: right;
+      margin-top: 10px;;
+    }
+  }
+  .van-cell{
+    padding: 10px 0;
+  }
+  .word_btn{
+    color:#3296fa;
+  }
+  .close_btn{
+    margin-top:5px;
+    color: #999;
+  }
+}
+.suggest-box{
+  /deep/ .van-cell{
+    padding: 10px 20px;
+    color: #999;
+    p{
+      span{
+        color: red;
+      }
+    }
+  }
+}
 </style>
