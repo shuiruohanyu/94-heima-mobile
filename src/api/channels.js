94d865fd6e32b0ce19de2f096cb38a9afc8b0d62
shuiruohanyu <shuiruohanyu@foxmail.com>
2020-03-22 14:26:50 +0800

    删除我的频道API

--- src/api/channels.js
+++ src/api/channels.js
@@ -46,3 +46,34 @@ export function getAllChannels () {
     url: '/channels'
   })
 }
+
+/****
+ *  删除频道API
+ * @params  id 作为删除频道的依据
+ * ****/
+export function delChannel (id) {
+  return new Promise(function (resolve, reject) {
+    // 有id 就可以直接从缓存中删除对应id的数据
+    // 删除频道时 无论如何都有数据的
+    const key = store.state.user.token ? CACHE_CHANNEL_V : CACHE_CHANNEL_T // key根据当前的登录状态来判断
+    const channels = JSON.parse(localStorage.getItem(key)) // 直接将本地缓存中的字符串转化成对象
+    // 第一种
+    // // 直接用filter删除  直接返回一个新数组 返回不等于id的内容
+    // channels = channels.filter(item => item.id !== id) // 得到一个新数组
+    // // 应该重新写入缓存
+    // localStorage.setItem(key, JSON.stringify(channels))
+    // // console.log(channels)
+    // 第二种
+    const index = channels.findIndex(item => item.id === id) // 找到对应频道的索引
+    if (index > -1) {
+      // 因为索引下标从0开始
+      channels.splice(index, 1) // 删除对应的下标元素
+      localStorage.setItem(key, JSON.stringify(channels)) // 重新写入缓存
+      // 如果执行成功了 我们应该 resolve()
+      resolve() // resolve 可以传参 也可以不传参
+    } else {
+      // 如果没有找到对应的下标
+      reject(new Error('没有找到对应的频道..'))
+    }
+  })
+}
