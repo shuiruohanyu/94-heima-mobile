f5edce022cecc96d3d7bf346d595c533080d2830
shuiruohanyu <shuiruohanyu@foxmail.com>
2020-03-23 15:29:14 +0800

    搜索结果-基础布局

--- src/views/search/result.vue
+++ src/views/search/result.vue
@@ -1,18 +1,72 @@
 <template>
-  <div class='container'>
+  <div class="container">
     <!-- 导航 显示返回箭头-->
     <!-- click-left点击左侧事件 -->
     <!-- $router.go(-1) $router.back() -->
-    <van-nav-bar title='搜索结果' left-arrow @click-left="$router.back()"></van-nav-bar>
+    <!-- 将 导航栏固定在顶部 -->
+    <van-nav-bar fixed title="搜索结果" left-arrow @click-left="$router.back()"></van-nav-bar>
+    <!-- 放置搜索结果列表 实现上拉加载  -->
+    <van-list>
+      <van-cell-group>
+        <!-- 文章列表结构 -->
+        <van-cell v-for="item in 20" :key="item">
+          <div class="article_item">
+            <h3 class="van-ellipsis">我们守望相助,从正月初一到三月十五</h3>
+            <!-- <div class="img_box">
+              <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+              <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+              <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+            </div> -->
+            <div class="img_box">
+              <van-image class="w100" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+            </div>
+            <div class="info_box">
+              <span>你像一阵风</span>
+              <span>8评论</span>
+              <span>10分钟前</span>
+            </div>
+          </div>
+        </van-cell>
+      </van-cell-group>
+    </van-list>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.container {
+  padding-top: 46px;
+  height: 100%;
+  overflow-y: auto;
+  box-sizing: border-box;
+}
+.article_item {
+  h3 {
+    font-weight: normal;
+    line-height: 2;
+  }
+  .img_box {
+    display: flex;
+    justify-content: space-between;
+    .w33 {
+      width: 33%;
+      height: 90px;
+    }
+    .w100 {
+      height: 180px;
+      width: 100%;
+    }
+  }
+  .info_box {
+    color: #999;
+    line-height: 2;
+    position: relative;
+    span {
+      padding-right: 10px;
+    }
+  }
+}
 </style>
