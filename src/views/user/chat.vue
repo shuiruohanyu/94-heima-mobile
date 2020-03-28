2cc5825681a3718531da48b9e0fc32378899600c
shuiruohanyu <shuiruohanyu@foxmail.com>
2020-03-28 14:27:25 +0800

    小智同学-实现聊天

--- src/views/user/chat.vue
+++ src/views/user/chat.vue
@@ -1,7 +1,7 @@
 <template>
    <div class="container">
     <van-nav-bar fixed left-arrow @click-left="$router.back()" title="小智同学"></van-nav-bar>
-    <div class="chat-list">
+    <div class="chat-list" ref="myList">
       <!-- 接下来 把list消息循环渲染到视图上 -->
       <!-- 认为 小智同学说的话 在左边  我说的话 在右边 -->
       <!-- name : xz  => 小智 左边  name !=xz  => 我  => 右边-->
@@ -29,6 +29,7 @@
 </template>
 
 <script>
+import Vue from 'vue'
 import XZImg from '@/assets/head.jpg' // 引入一张图片
 import { mapState } from 'vuex' // vuex辅助函数
 import io from 'socket.io-client' // 引入socke.io的客户端
@@ -67,9 +68,27 @@ export default {
     this.socket.on('message', data => {
       //  data就是 客户服务器回复的消息 认为接收的消息都是 小智同学回复的
       this.list.push({ ...data, name: 'xz' }) // 加到列表中
+      this.scrollBottom() // 接消息之后
     })
   },
   methods: {
+    // 将内容滚动到底部 设置 滚动条的scrollTop(滚动条距离顶部的长度) = scrollHeight(整个容器的实际高度W)
+    scrollBottom () {
+      // vuejs  数据变化   => 异步的  => 视图更新
+      // vuejs 内部的渲染 是异步的 不是同步的
+      // 我们需要等到 异步更新完成之后 才去执行  滚动
+      // this.$nextTick 此函数 会在 上一次数据更新 并且完成页面渲染之后执行
+      // this.$nextTick(() => {
+      //   //  在这个回调函数中  上一次的数据更新结果 已经 反映到了视图上 视图已经更新了
+      //   this.$refs.myList.scrollTop = this.$refs.myList.scrollHeight // 将滚动条滚动到最底部
+      //   // 此时表示 滚动滚动条的时候 视图上数据已经是最新了
+      // })
+      // 这是使用组件实例的方式
+      Vue.nextTick(() => {
+        // 这是使用全局对象的方式 也表示 上一次数据更新 完成页面渲染之后 执行 该函数
+        this.$refs.myList.scrollTop = this.$refs.myList.scrollHeight // 将滚动条滚动到最底部
+      })
+    },
     // 发送消息方法
     async send () {
       // 调用websocket发出一个消息
@@ -91,7 +110,14 @@ export default {
       // 发送完消息之后
       this.value = '' // 清空消息内容
       this.loading = false // 关掉状态
+      this.scrollBottom() // 发消息之后
     }
+  },
+  // 实例销毁前
+  beforeDestroy () {
+    // 需要在实例销毁之前  把websocket连接给关掉
+    // ws.close() 原生    socket.io  close()
+    this.socket.close() // 直接关闭连接
   }
 }
 </script>
